7324490ae70dfda3a7e9e48b6cfe32cb20a9d33d
Austin Kelsay <austinkelsay@yahoo.com>
2026-01-11 13:14:06 -0600

    import alias fix

--- src/__tests__/components/OnboardingWelcome.test.tsx
+++ src/__tests__/components/OnboardingWelcome.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { OnboardingWelcome } from '../../components/OnboardingWelcome';
+import { OnboardingWelcome } from '@/components/OnboardingWelcome';
 
 describe('OnboardingWelcome Component', () => {
   const mockOnGetStarted = jest.fn();
